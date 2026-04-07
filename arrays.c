9f7cc92f26eb978f3dad840b0d1de5d2bc7744f8
manasapilli9676 <pillymanasa@gmail.com>
2026-04-07 11:08:47 +0530

    .

--- arrays.c
+++ arrays.c
@@ -1,8 +1,13 @@
-#include<stdio.h>
-int main()
-{
-int n=5,arr[5];
-for(int i=0;i<n;i++){
-    scanf("%d",arr[i]);
-}
+#include <stdio.h>
+
+int main() {
+    int n=5;
+    int arr[5];
+    for(int i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+
+    for(int i=0;i<n;i++)
+    printf("%d/n",arr[i]);
+
+    return 0;
 }
