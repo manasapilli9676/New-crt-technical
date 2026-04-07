5c90c1b38a76065e93fc2b6fb8130442e50d133d
manasapilli9676 <pillymanasa@gmail.com>
2026-04-07 11:24:00 +0530

    .

--- smallinarray.c
+++ smallinarray.c
@@ -1,4 +1,18 @@
 #include<stdio.h>
 int main(){
+int n=5,arr[5];
+for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+for(int i=0;i<n;i++){
+    printf("%d",arr[i]);
+}
+int min=arr[0];
+for(int i=0;i<n;i++){
+if(min>arr[i]){
+    min=arr[i];
+}
 
+}
+printf("%d",min);
 }
