d6506e5cc7cc7b8674fd6f2fd7870df809516c2d
manasapilli9676 <pillymanasa@gmail.com>
2026-04-11 12:37:30 +0530

    .

---
+++ flip.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int num=5;
+    int res=(num&(-1));
+    printf("%d",res);
+    return 0;
+}
---
+++ suedo1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int n=3;
+    for(int i=0;i<=n;i++){
+        printf("hello world");
+    }
+    return 0;
+}
