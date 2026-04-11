4325fcbfa0af4437fd99d1ba14fd57de0be70071
manasapilli9676 <pillymanasa@gmail.com>
2026-04-11 11:41:29 +0530

    .

--- frequeny.c
+++ frequeny.c
@@ -3,7 +3,7 @@ int main(){
     int arr[7]={1,3,4,1,4,3,5};
     int new=0;
     for(int i=0;i<7;i++){
-        printf("%d",arr[i]);
+       // printf("%d",arr[i]);
         new=new^arr[i];
     }
     printf("%d",new);
