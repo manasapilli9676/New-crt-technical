8a4fcaa1c577d54d9e5cb320294b42a69571dd83
manasapilli9676 <pillymanasa@gmail.com>
2026-04-12 23:04:50 +0530

    .

---
+++ findalpha.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<ctype.h>
+int main(){
+    char examp[]="narmadha143";
+    int alpha=0,number=0;
+    for(int i =0;i<11;i++){
+    if(isalpha(examp[i])){
+    alpha++;
+    }
+    else{
+        number++;
+    }
+ }
+ printf("alphabets%d\n",alpha);
+ printf("numbers%d\n",number);
+ return 0;
+}
+
